3327cd966318434fe9ba3dcba12afb23f3eb1fce
Leonardo <jleonardolopez@hotmail.com>
2023-04-03 15:53:41 +1000

    no sandbox add

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const qrcode = require("qrcode-terminal");
 
 const { Client, LocalAuth } = require("whatsapp-web.js");
-const client = new Client({ authStrategy: new LocalAuth() });
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ["--no-sandbox"],
+  },
+});
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
